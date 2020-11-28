3fe2e9d83b9b37d3a5b78a5a069935dcdaafad2a
Darius Couchard <darius.couchard@hotmail.com>
2020-11-28 12:27:55 +0100

    Added tests for linear value interpolation

--- test/core/test_types.cpp
+++ test/core/test_types.cpp
@@ -37,3 +37,34 @@ TEST_F(TypeTest, TestVectorType)
     ASSERT_FLOAT_EQ(d_x, 1.0);
     ASSERT_FLOAT_EQ(d_y, -2.0);
 }
+
+TEST_F(TypeTest, TestInterplationFloat)
+{
+    core::vec2f_t pos(1.0, 5.0);
+    core::vec2f_t new_pos(2.0, 3.0);
+
+    core::value_t delta_pos_ptr = pos.get_delta(&new_pos);
+
+    core::value_t middle_pos = pos.interp(delta_pos_ptr.get(), 0.5);
+
+    ASSERT_FLOAT_EQ(middle_pos->cast<core::vec2f_t>()->x(), 1.5);
+    ASSERT_FLOAT_EQ(middle_pos->cast<core::vec2f_t>()->y(), 4.0);
+}
+
+TEST_F(TypeTest, TestInterpolationInt)
+{
+    core::vec2i_t grid(5, 5);
+    core::vec2i_t new_grid(10, 10);
+
+    core::value_t delta_grid_ptr = grid.get_delta(&new_grid);
+
+    core::value_t middle_grid_1 = grid.interp(delta_grid_ptr.get(), 0.5);
+
+    ASSERT_EQ(middle_grid_1->cast<core::vec2i_t>()->x(), 7);
+    ASSERT_EQ(middle_grid_1->cast<core::vec2i_t>()->y(), 7);
+
+    core::value_t middle_grid_2 = grid.interp(delta_grid_ptr.get(), 0.6);
+
+    ASSERT_EQ(middle_grid_2->cast<core::vec2i_t>()->x(), 8);
+    ASSERT_EQ(middle_grid_2->cast<core::vec2i_t>()->y(), 8);
+}

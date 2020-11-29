4f2658470bff71869c5ae6126448ef3d2764bc99
Darius Couchard <darius.couchard@hotmail.com>
2020-11-29 23:41:07 +0100

    started Server networking

--- include/net/server.hpp
+++ include/net/server.hpp
@@ -1,1 +1,23 @@
 #pragma once
+#include <boost/asio.hpp>
+
+class Server
+{
+public:
+    Server();
+
+    ~Server();
+
+private:
+
+    /**
+     * Blocking function that will start all the working threads of the server
+     * and then join them once their work is done.
+     */
+    bool start_threads(std::uint16_t thread_count);
+
+    void worker_thread(std::shared_ptr<boost::asio::io_service> service);
+
+    std::shared_ptr<boost::asio::io_service> _service;
+    std::shared_ptr<boost::asio::io_service::work> _work;
+};
